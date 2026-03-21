c8ac0107232a8fd0f6e066cc9c75c74bce92de72
Alex Csiszar <csiszaralex@gmail.com>
2026-03-21 23:51:42 +0100

    chore(repo): add @commitlint/config-nx-scopes for enhanced commit linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
+import { utils } from '@commitlint/config-nx-scopes';
+
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [...(await utils.getProjects(ctx)), 'repo', 'release', 'ci'],
+    ],
+    'scope-empty': [2, 'never'],
+  },
 };
